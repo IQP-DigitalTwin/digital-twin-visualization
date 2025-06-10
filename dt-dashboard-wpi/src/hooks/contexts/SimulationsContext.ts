6a3d14ff93a7b665dfd0d4f2a74bc1b716dd94ec
Kai Davidson <kai@davidson.to>
2025-06-10 10:22:44 +0200

    Moved working project to root directory

--- dt-dashboard-wpi/src/hooks/contexts/SimulationsContext.ts
+++
@@ -1,6 +0,0 @@
-import { Simulation } from "@/repositories/Simulation/ISimulationRepository";
-import { createContext } from "react";
-
-export default createContext<
-  [Simulation[], (simulations: Simulation[]) => void]
->([[], () => {}]);
--- dt-dashboard-wpi/src/hooks/contexts/WeatherContext.tsx
+++
@@ -1,4 +0,0 @@
-import { Weather } from "@/types/Weather";
-import { createContext } from "react";
-
-export default createContext<Weather | null>(null);
